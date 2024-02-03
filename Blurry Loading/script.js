f8a63bca86662175af03d68f2c15469f74c9f231
GCortesGustavo <gustavocortes934@gmail.com>
2024-02-03 16:29:47 -0300

    Modificando cositas

--- Blurry Loading/script.js
+++ Blurry Loading/script.js
@@ -5,6 +5,10 @@ let load = 0
 
 let int = setInterval(blurring, 30)
 
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+}
+
 function blurring() {
     load++
 
@@ -17,6 +21,3 @@ function blurring() {
     bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
 }
 
-const scale = (num, in_min, in_max, out_min, out_max) => {
-    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
-  }
