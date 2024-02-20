89ac65c5ed341941a87e8d78dceaf56d2a0647f0
GCortesGustavo <gustavocortes934@gmail.com>
2024-02-20 18:14:13 -0300

    Se agregan comentarios

--- button-ripple-effect/script.js
+++ button-ripple-effect/script.js
@@ -1,25 +1,31 @@
 const buttons = document.querySelectorAll(".ripple")
 
 
+// Itera sobre cada elemento de la lista de botones.
 buttons.forEach(button => {
     button.addEventListener("click", function (e) {
+        // Obtiene las coordenadas X e Y del clic del ratón en relación con la página.
         const x = e.pageX
         const y = e.pageY
 
+        // Obtiene la posición superior e izquierda del botón en relación con su contenedor.
         const buttonTop = e.target.offsetTop
         const buttonLeft = e.target.offsetLeft
         
+        // Calcula la posición interna del clic del ratón en relación con el botón.
         const xInside = x - buttonLeft
         const yInside = y - buttonTop
 
+        // Crea un elemento span para representar el efecto de onda.
         const circle = document.createElement("span")
         circle.classList.add("circle")
         circle.style.top = yInside + "px"
         circle.style.left = xInside + "px"
 
-
+        // Añade el elemento span al botón.
         this.appendChild(circle)
 
+        // Elimina el elemento span después de 500 milisegundos.
         setTimeout(() => circle.remove(), 500)
     })
 })
